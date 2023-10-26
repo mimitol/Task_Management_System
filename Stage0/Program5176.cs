92203ec773edaf0dee3abcb3493438ffcc26108a
mimitol <mimiti99160@gmail.com>
2023-10-26 12:48:40 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program5176.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome5176();
+        Welcome5571();
+        Console.ReadKey();
+    }
+
+    private static void Welcome5176()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+    private static void Welcome5571() { }
+}
