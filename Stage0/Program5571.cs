311c741e499c5579c69ae450363788d2bf7e3186
yael2544 <yael0556781683@gmail.com>
2023-10-26 13:54:06 +0300

    changes 2

---
+++ Stage0/Program5571.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Welcome5571()
+        {
+            Console.WriteLine("Welcome5571");
+        }
+    }
+}
