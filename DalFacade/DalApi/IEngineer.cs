874f90b78b5c54e3cfc359919a3107fe7143dcf8
yael2544 <yael0556781683@gmail.com>
2023-11-09 13:06:19 +0200

    interface of enigneer and dependency

---
+++ DalFacade/DalApi/IEngineer.cs
@@ -0,0 +1,12 @@
+﻿namespace DalApi;
+
+using do;
+
+public interface IEngineer
+{
+    int Create(T Enigneer); //Creates new entity object in DAL
+    Enigneer? Read(int id); //Reads entity object by its ID
+    List<Enigneer> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(Enigneer item); //Updates entity object
+    void Delete(int id); //Deletes an object by its Id
+}
