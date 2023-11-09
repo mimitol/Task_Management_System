f0823135eda7e39884b460b8a717b76ad860756d
yael2544 <yael0556781683@gmail.com>
2023-11-09 11:37:39 +0200

    Defining Entities: Dependencies and Engineer

---
+++ DalFacade/DO/Dependency.cs
@@ -0,0 +1,22 @@
+﻿namespace DO;
+
+/// <summary>
+/// Dependenc Entity
+/// </summary>
+/// <param name="id">Dependency ID number</param>
+/// <param name="dependentTask">ID number of pending task</param>
+/// <param name="dependsOnTask">Previous assignment ID number</param>
+public record Dependency
+(
+    int id,
+    int dependentTask,
+    int dependsOnTask
+)
+{
+    public Dependency() : this(0) { } //empty ctor
+    public Dependency(int dependentTask, int dependsOnTask) //full ctor
+    {
+        this.dependentTask = dependentTask;
+        this.dependsOnTask = dependsOnTask;
+    }
+}
---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,27 @@
+﻿namespace DO;
+/// <summary>
+/// Engineer Entity
+/// </summary>
+/// <param name="id">Engineer ID number</param>
+/// <param name="name">Engineer's name</param>
+/// <param name="email">Email</param>
+/// <param name="level">Engineer level</param>
+/// <param name="cost">cost per hour</param>
+public record Engineer
+(
+    int id,
+    string? name,
+    string? email,
+    EngineerExperience level,
+    double cost
+)
+{
+    public Engineer() :this(0) { } //empty ctor
+    public Engineer(string? name, string? email, EngineerExperience level, double cost) //full ctor
+    {
+        this.name = name;
+        this.email = email;
+        this.level = level;
+        this.cost = cost;
+    }
+}
