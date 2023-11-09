e59c449080e152b745f58d70b35c50bc11a2db24
yael2544 <yael0556781683@gmail.com>
2023-11-09 13:36:43 +0200

    Implementation dependency and enigneer

--- DalFacade/DalApi/IEngineer.cs
+++ DalFacade/DalApi/IEngineer.cs
@@ -4,7 +4,7 @@ using do;
 
 public interface IEngineer
 {
-    int Create(T Enigneer); //Creates new entity object in DAL
+    int Create(Enigneer item); //Creates new entity object in DAL
     Enigneer? Read(int id); //Reads entity object by its ID
     List<Enigneer> ReadAll(); //stage 1 only, Reads all entity objects
     void Update(Enigneer item); //Updates entity object
---
+++ DalList/EnigneerImplementation.cs
@@ -0,0 +1,29 @@
+﻿namespace Dal;
+
+using DalApi;
+using DO;
+
+public class EnigneerImplementation : IEnigneer
+{
+    public int Create(Enigneer item)
+    {
+        throw new NotImplementedException();
+    }
+    public Enigneer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public List<Enigneer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+    public void Update(Enigneer item)
+    {
+        throw new NotImplementedException();
+    }
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
