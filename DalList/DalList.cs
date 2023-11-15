927328ad158e4189138ec6c3e99cc9f78b1f5c13
yael2544 <yael0556781683@gmail.com>
2023-11-15 16:12:16 +0200

    end stage2 chapter 4

---
+++ DalList/DalList.cs
@@ -0,0 +1,11 @@
+﻿namespace Dal;
+using DalApi;
+public class DalList : IDal
+{
+    public ITask Task => new TaskImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public IDependency Dependency => new DependencyImplementation();
+
+}
