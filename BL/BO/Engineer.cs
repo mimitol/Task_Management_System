3017fef77232eba675c8989bd7668b6294ae0367
mimitol <mimiti99160@gmail.com>
2024-01-07 09:04:15 +0200

    באמצע מימוש READ של מהנדס

--- BL/BO/Engineer.cs
+++ BL/BO/Engineer.cs
@@ -19,10 +19,10 @@ namespace BO
     {
         public int Id { get; init; }
         public string? Name { get; init; }
-        public string? Email { get; init; }
+        public string? Email { get; set; }
         public EngineerExperience Level { get; init; }
         public double Cost { get; init; }
-        public TaskInList EngineersTask { get; set; }
+        public TaskInList? EngineersTask { get; set; }
         public override string ToString() => this.ToStringProperty();
 
     }
--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -18,35 +18,35 @@ namespace BlImplementation
         {
             if (boEngineer.Id < 0)
                 throw new BO.BlInvalidPropertyException("you entered an invalid Id");
-            if(boEngineer.Cost<0)
+            if (boEngineer.Cost < 0)
                 throw new BO.BlInvalidPropertyException("you entered an invalid cost");
-            if(boEngineer.Name=="")
+            if (boEngineer.Name == "")
                 throw new BO.BlInvalidPropertyException("you entered an invalid name");
             if (!Regex.IsMatch(boEngineer.Email, @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"))
                 throw new BO.BlInvalidPropertyException("you entered an invalid email");
-            DO.Engineer doEngineer = new DO.Engineer(boEngineer.Id, boEngineer.Name, boEngineer.Email,(DO.EngineerExperience) boEngineer.Level, boEngineer.Cost);
+            DO.Engineer doEngineer = new DO.Engineer(boEngineer.Id, boEngineer.Name, boEngineer.Email, (DO.EngineerExperience)boEngineer.Level, boEngineer.Cost);
             try
             {
                 return _dal.Engineer.Create(doEngineer);
             }
             catch (DO.DalAlreadyExistsException exception)
             {
-                throw new BO.BlAlreadyExistsException($"Engineer with id:{boEngineer.Id} already Exist",exception);
+                throw new BO.BlAlreadyExistsException($"Engineer with id:{boEngineer.Id} already Exist", exception);
             }
-            
+
         }
 
         public void Delete(int id)
         {
-            if(_dal.Task.Read(t => t.EngineerId == id)!=null)
-                throw new BO.BlDeletionImpossible ("you can't delete an engineer who's working on a task");
+            if (_dal.Task.Read(t => t.EngineerId == id) != null)
+                throw new BO.BlDeletionImpossible("you can't delete an engineer who's working on a task");
             try
             {
                 _dal.Engineer.Delete(id);
             }
-            catch(DO.DalDoesNotExistException exception)
+            catch (DO.DalDoesNotExistException exception)
             {
-                throw new BO.BlDoesNotExistException($"Engineer with id:{boEngineer.Id} does not Exist", exception);
+                throw new BO.BlDoesNotExistException($"Engineer with id:{id} does not Exist", exception);
             }
         }
 
@@ -54,16 +54,32 @@ namespace BlImplementation
         {
             try
             {
-                 DO.Engineer doEngineer=_dal.Engineer.Read(id);
-                BO.Engineer boEngineer = new BO.Engineer
-                {
-                    Id = doEngineer.Id, Name = doEngineer.Name, Email = doEngineer.Email,
-                }
+                DO.Engineer doEngineer = _dal.Engineer.Read(id);
             }
             catch (DO.DalDoesNotExistException exception)
             {
                 throw new BO.BlAlreadyExistsException($"Engineer with id:{id} Does Not Exist", exception);
             }
+
+            DO.Task? doEngineersTask = from t in _dal.Task.ReadAll()
+                                       where t.EngineerId == id
+                                       select t;
+            BO.Engineer boEngineer = new BO.Engineer
+            {
+                Id = doEngineer.Id,
+                Name = doEngineer.Name,
+                Email = doEngineer.Email,
+                Level = (BO.EngineerExperience)doEngineer.Level,
+                Cost = doEngineer.Cost,
+                EngineersTask = new BO.TaskInList
+                {
+                    Id = doEngineersTask.Id,
+                    Description = doEngineersTask.Description,
+                    Alias = doEngineersTask.Alias
+                }
+            };
+
+            return b
         }
 
         public IEnumerable<DO.Engineer> ReadAll(Func<DO.Engineer, bool>? filter = null)
